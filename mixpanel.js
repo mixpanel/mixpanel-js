70472d180a0d7ff53b8e4c21ad7ea2197541786c
teddddd <ted@mixpanel.com>
2014-08-22 15:47:19 -0700

    store notification ID in cookie when marking as shown

--- mixpanel.js
+++ mixpanel.js
@@ -60,7 +60,16 @@ Globals should be all caps
 // This key is deprecated, but we want to check for it to see whether aliasing is allowed.
 /** @const */       , PEOPLE_DISTINCT_ID_KEY    = "$people_distinct_id"
 /** @const */       , ALIAS_ID_KEY              = "__alias"
-/** @const */       , RESERVED_PROPERTIES       = [SET_QUEUE_KEY, SET_ONCE_QUEUE_KEY, ADD_QUEUE_KEY, APPEND_QUEUE_KEY, PEOPLE_DISTINCT_ID_KEY, ALIAS_ID_KEY];
+/** @const */       , CAMPAIGN_IDS_KEY          = "__cmpns"
+/** @const */       , RESERVED_PROPERTIES       = [
+                        SET_QUEUE_KEY,
+                        SET_ONCE_QUEUE_KEY,
+                        ADD_QUEUE_KEY,
+                        APPEND_QUEUE_KEY,
+                        PEOPLE_DISTINCT_ID_KEY,
+                        ALIAS_ID_KEY,
+                        CAMPAIGN_IDS_KEY
+                    ];
 
 /*
  * Dynamic... constants? Is that an oxymoron?
@@ -2939,6 +2948,11 @@ Globals should be all caps
         _.bind_instance_methods(this);
 
         this.mixpanel = mixpanel_instance;
+        this.cookie = new MixpanelCookie({
+            'cookie_name':       this.mixpanel['config']['token'] + '_cmpns',
+            'cookie_expiration': 1
+        });
+        this.shown_campaigns = this.cookie['props'][CAMPAIGN_IDS_KEY] || (this.cookie['props'][CAMPAIGN_IDS_KEY] = []);
 
         this.campaign_id = _.escapeHTML(notif_data['id']);
         this.message_id  = _.escapeHTML(notif_data['message_id']);
@@ -3003,7 +3017,11 @@ Globals should be all caps
 
         MixpanelLib._Notification.prototype.dismiss = _.safewrap(function() {
             if (this.campaign_id) {
-                // mark notification shown
+                // mark notification shown (local cache)
+                this.shown_campaigns.push(this.campaign_id);
+                this.cookie.save();
+
+                // mark notification shown (mixpanel property)
                 this.mixpanel.people.append({
                     $campaigns: this.campaign_id,
                     $notifications: {
@@ -3086,7 +3104,7 @@ Globals should be all caps
             var self = this;
 
             // no possibility to double-display
-            if (this.shown) {
+            if (this.shown || this.shown_campaigns.indexOf(this.campaign_id) >= 0) {
                 return;
             }
             this.shown = true;
