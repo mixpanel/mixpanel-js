e5699d93f1b76e393f7065b244546da04e1bb2ad
teddddd <ted@mixpanel.com>
2014-08-25 12:54:00 -0700

    explicit z-indexing of notification positioned elements to reduce chance of collisions with stacking of other page elements

--- mixpanel.js
+++ mixpanel.js
@@ -3214,25 +3214,31 @@ Globals should be all caps
             };
 
             this._inject_styles({
+                '#mixpanel-notification-wrapper': {
+                    'position': 'absolute',
+                    'z-index': '50000'
+                },
                 '#mixpanel-notification-overlay': {
                     'position': 'fixed',
+                    'z-index': '50010',
                     'top': '0',
                     'left': '0',
                     'width': '100%',
                     'height': '100%',
                     'overflow': 'auto',
                     'text-align': 'center',
-                    'z-index': '10000',
                     'font-family': '"Helvetica", "Arial", sans-serif',
                     '-webkit-font-smoothing': 'antialiased'
                 },
                 '#mixpanel-notification-bgwrapper': {
                     'position': 'relative',
+                    'z-index': '50020',
                     'width': '100%',
                     'height': '100%'
                 },
                 '#mixpanel-notification-bg': {
                     'position': 'fixed',
+                    'z-index': '50030',
                     'top': '0',
                     'left': '0',
                     'width': '100%',
@@ -3248,6 +3254,7 @@ Globals should be all caps
                 },
                 '#mixpanel-notification-thumbnail': {
                     'position': 'absolute',
+                    'z-index': '50040',
                     'right': '65px',
                     'width': MixpanelLib._Notification.THUMB_IMG_SIZE + 'px',
                     'height': MixpanelLib._Notification.THUMB_IMG_SIZE + 'px',
@@ -3258,6 +3265,7 @@ Globals should be all caps
                 },
                 '#mixpanel-notification': {
                     'position': 'absolute',
+                    'z-index': '50050',
                     'right': '0',
                     'width': MixpanelLib._Notification.NOTIF_WIDTH + 'px',
                     'margin-right': MixpanelLib._Notification.NOTIF_MARGIN + 'px',
