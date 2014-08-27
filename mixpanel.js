a06eb3151475fa2775a2a03a207ffa3fbf0f6669
teddddd <ted@mixpanel.com>
2014-08-27 16:59:36 -0700

    ensure that loaded callback always gets called for main lib

--- mixpanel.js
+++ mixpanel.js
@@ -2956,6 +2956,7 @@ Globals should be all caps
             } else if (token) {
                 // intialize the main mixpanel lib
                 instance = create_mplib(token, config, PRIMARY_INSTANCE_NAME);
+                instance._loaded();
             }
 
             window[PRIMARY_INSTANCE_NAME] = mixpanel = instance;
