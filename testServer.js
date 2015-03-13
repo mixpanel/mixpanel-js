c0747982df59a9a0090f31083346306c764fd5cb
sandorfr <cyprien.autexier@gmail.com>
2015-03-14 00:05:36 +0100

    remove unused console.log and add missing semicolons

--- testServer.js
+++ testServer.js
@@ -1,14 +1,13 @@
-var express = require('express')
-var cookieParser = require('cookie-parser')
-var app = express()
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use('/tests', express.static(__dirname+"/tests"));
 app.use( express.static(__dirname+"/js"));
 
 app.get('/tests/cookie_included/:cookieName', function (req, res) {
-    console.log(JSON.stringify(req.cookies));
     if (req.cookies && req.cookies[req.params.cookieName]){
         res.json(1);
     }
@@ -22,6 +21,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Mixpanel test app listening at http://%s:%s', host, port);
 
 });
