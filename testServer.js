03a7e970c4f6129701a2f2a8f42e396edaa54725
teddddd <ted@mixpanel.com>
2015-03-27 15:58:04 -0700

    redirect test server root to tests/

--- testServer.js
+++ testServer.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(logger('dev'));
 
 app.use('/tests', express.static(__dirname + "/tests"));
-app.get('/tests/cookie_included/:cookieName', function (req, res) {
+app.get('/tests/cookie_included/:cookieName', function(req, res) {
     if (req.cookies && req.cookies[req.params.cookieName]) {
         res.json(1);
     } else {
@@ -18,6 +18,9 @@ app.get('/tests/cookie_included/:cookieName', function (req, res) {
     }
 });
 app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.redirect(301, '/tests/');
+});
 
 var server = app.listen(3000, function () {
   console.log('Mixpanel test app listening on port %s', server.address().port);
