7c0d632a01cf2785241ea13ba62689407a0475f5
teddddd <ted@mixpanel.com>
2015-08-07 18:19:14 +0000

    add closure compiler jar for building with correct version

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+
 if [ -z "$1" ]; then
-    echo "Please supply a path to the Google Closure Compiler .jar"
-    exit 1
+    COMPILER=vendor/closure-compiler/compiler.jar
+else
+    COMPILER=$1
 fi
 
-java -jar $1 --js mixpanel.js --js_output_file mixpanel.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function() {
+java -jar $COMPILER --js mixpanel.js --js_output_file mixpanel.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function() {
 %output%
 })();"
 
-java -jar $1 --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.js --compilation_level ADVANCED_OPTIMIZATIONS
+java -jar $COMPILER --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.js --compilation_level ADVANCED_OPTIMIZATIONS
 
-java -jar $1 --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.test.js --compilation_level ADVANCED_OPTIMIZATIONS --define='MIXPANEL_LIB_URL="../mixpanel.min.js"'
+java -jar $COMPILER --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.test.js --compilation_level ADVANCED_OPTIMIZATIONS --define='MIXPANEL_LIB_URL="../mixpanel.min.js"'
