af44abb37ee9f7cd2e888342a61b61bb75f7972c
teddddd <ted@mixpanel.com>
2015-10-01 12:23:14 -0400

    more verbose build script output

--- build.sh
+++ build.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+echo 'Building module and globals bundles'
+
 ./node_modules/.bin/rollup -i src/loader-module.js -f amd -o build/mixpanel.amd.js
 ./node_modules/.bin/rollup -i src/loader-module.js -f cjs -o build/mixpanel.cjs.js
 ./node_modules/.bin/rollup -i src/loader-module.js -f umd -o build/mixpanel.umd.js -n mixpanel
 ./node_modules/.bin/rollup -i src/loader-globals.js -f iife -o build/mixpanel.globals.js -n mixpanel
+
+
+echo 'Minifying globals build and snippets'
 
 if [ -z "$1" ]; then
     COMPILER=vendor/closure-compiler/compiler.jar
 else
     COMPILER=$1
 fi
-
 java -jar $COMPILER --js mixpanel.js --js_output_file mixpanel.min.js --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function() {
 %output%
 })();"
-
 java -jar $COMPILER --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.js --compilation_level ADVANCED_OPTIMIZATIONS
-
 java -jar $COMPILER --js mixpanel-jslib-snippet.js --js_output_file mixpanel-jslib-snippet.min.test.js --compilation_level ADVANCED_OPTIMIZATIONS --define='MIXPANEL_LIB_URL="../mixpanel.min.js"'
+
+
+echo 'Bundling module-loader examples'
 
 pushd examples/commonjs-browserify; npm install && npm run build; popd
 pushd examples/es2015-babelify; npm install && npm run build; popd
