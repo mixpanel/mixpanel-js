b4cf2c5cb900fbb102370c01553e9a70f4c6e2d0
teddddd <ted@mixpanel.com>
2016-12-09 01:23:21 +0000

    really build 2.9.17

--- build/mixpanel.umd.js
+++ build/mixpanel.umd.js
@@ -6,7 +6,7 @@
 
     var Config = {
         DEBUG: false,
-        LIB_VERSION: '2.9.16'
+        LIB_VERSION: '2.9.17'
     };
 
     // since es6 imports are static and we run unit tests from the console, window won't be defined when importing this file
--- examples/es2015-babelify/bundle.js
+++ examples/es2015-babelify/bundle.js
@@ -574,7 +574,7 @@ Object.defineProperty(exports, '__esModule', {
 });
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.9.16'
+    LIB_VERSION: '2.9.17'
 };
 
 exports['default'] = Config;
