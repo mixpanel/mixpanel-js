4e77ebbaf342cfab4d95e7c7686c4963cb05ad76
teddddd <ted@mixpanel.com>
2018-11-05 22:20:14 +0000

    api-actions cosmetic cleanup

--- src/api-actions.js
+++ src/api-actions.js
@@ -1,16 +1,19 @@
-import { _ } from './utils';
 /* eslint camelcase: "off" */
-/** @const */   var SET_ACTION                = '$set';
-/** @const */   var SET_ONCE_ACTION           = '$set_once';
-/** @const */   var UNSET_ACTION              = '$unset';
-/** @const */   var ADD_ACTION                = '$add';
-/** @const */   var APPEND_ACTION             = '$append';
-/** @const */   var UNION_ACTION              = '$union';
-/** @const */   var REMOVE_ACTION             = '$remove';
-/** @const */   var DELETE_ACTION             = '$delete';
 
+import { _ } from './utils';
+
+/** @const */ var SET_ACTION      = '$set';
+/** @const */ var SET_ONCE_ACTION = '$set_once';
+/** @const */ var UNSET_ACTION    = '$unset';
+/** @const */ var ADD_ACTION      = '$add';
+/** @const */ var APPEND_ACTION   = '$append';
+/** @const */ var UNION_ACTION    = '$union';
+/** @const */ var REMOVE_ACTION   = '$remove';
+/** @const */ var DELETE_ACTION   = '$delete';
+
+// Common internal methods for mixpanel.people and mixpanel.group APIs.
+// These methods shouldn't involve network I/O.
 var apiActions = {
-// common internal methods for mixpanel.people and mixpanel.group APIs, these methods shouldn't involve network IO.
     set_action: function(prop, to) {
         var data = {};
         var $set = {};
@@ -100,4 +103,14 @@ var apiActions = {
     }
 };
 
-export { SET_ACTION, SET_ONCE_ACTION, UNSET_ACTION, ADD_ACTION, APPEND_ACTION, UNION_ACTION, REMOVE_ACTION, DELETE_ACTION, apiActions };
+export {
+    SET_ACTION,
+    SET_ONCE_ACTION,
+    UNSET_ACTION,
+    ADD_ACTION,
+    APPEND_ACTION,
+    UNION_ACTION,
+    REMOVE_ACTION,
+    DELETE_ACTION,
+    apiActions
+};
