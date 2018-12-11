fdac547576f2ab4ab985fd69a8c8c8041e21d096
Piyush Goyal <piyush.goyal@thoughtspot.com>
2018-12-11 17:32:09 +0530

    Configure mixpanel track api to use POST.
    Current library only supports GET based http requests for /track calls, even though backend server side supports POST api as well. Due to a large number of properties, we are running into the URL length limits, breaking our collectors. So adding support to enable POST based /track calls

--- src/mixpanel-core.js
+++ src/mixpanel-core.js
@@ -949,11 +949,13 @@ MixpanelLib.prototype._prepare_callback = function(callback, data) {
     }
 };
 
-MixpanelLib.prototype._send_request = function(url, data, callback) {
+MixpanelLib.prototype._send_request = function(url, data, callback, http_request_type) {
     if (ENQUEUE_REQUESTS) {
         this.__request_queue.push(arguments);
         return;
     }
+
+    http_request_type = http_request_type || 'GET';
 
     // needed to correctly format responses
     var verbose_mode = this.get_config('verbose');
@@ -976,16 +978,21 @@ MixpanelLib.prototype._send_request = function(url, data, callback) {
 
     data['ip'] = this.get_config('ip')?1:0;
     data['_'] = new Date().getTime().toString();
-    url += '?' + _.HTTPBuildQuery(data);
 
     if ('img' in data) {
         var img = document.createElement('img');
+        url += '?' + _.HTTPBuildQuery(data);
         img.src = url;
         document.body.appendChild(img);
     } else if (USE_XHR) {
         try {
             var req = new XMLHttpRequest();
-            req.open('GET', url, true);
+            if(http_request_type === 'GET') {
+                url += '?' + _.HTTPBuildQuery(data);
+                req.open('GET', url, true);
+            } else {
+                req.open('POST', url, true);
+            }
 
             var headers = this.get_config('xhr_headers');
             _.each(headers, function(headerValue, headerName) {
@@ -1025,7 +1032,11 @@ MixpanelLib.prototype._send_request = function(url, data, callback) {
                     }
                 }
             };
-            req.send(null);
+            if(http_request_type === 'GET') {
+                req.send(null);
+            } else {
+                req.send(_.HTTPBuildQuery(data));
+            }
         } catch (e) {
             console.error(e);
         }
@@ -1034,6 +1045,7 @@ MixpanelLib.prototype._send_request = function(url, data, callback) {
         script.type = 'text/javascript';
         script.async = true;
         script.defer = true;
+        url += '?' + _.HTTPBuildQuery(data);
         script.src = url;
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(script, s);
@@ -1197,6 +1209,7 @@ MixpanelLib.prototype.track = addOptOutCheckMixpanelLib(function(event_name, pro
         'event': event_name,
         'properties': properties
     };
+    var http_request_type = this.get_config('track_request_type') || 'GET';
     var truncated_data = _.truncate(data, 255);
     var json_data      = _.JSONEncode(truncated_data);
     var encoded_data   = _.base64Encode(json_data);
@@ -1207,7 +1220,8 @@ MixpanelLib.prototype.track = addOptOutCheckMixpanelLib(function(event_name, pro
     this._send_request(
         this.get_config('api_host') + '/track/',
         { 'data': encoded_data },
-        this._prepare_callback(callback, truncated_data)
+        this._prepare_callback(callback, truncated_data),
+        http_request_type
     );
 
     return truncated_data;
