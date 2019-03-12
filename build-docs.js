638c3d666b08709e74be3028eed1c051cfd9847f
teddddd <ted@mixpanel.com>
2019-03-12 13:27:44 -0700

    fill out the rest of the existing API doc fields

--- build-docs.js
+++ build-docs.js
@@ -1,5 +1,6 @@
 const dox = require(`dox`);
 const fs = require(`fs`);
+const {trim} = require(`lodash`);
 const path = require(`path`);
 
 
@@ -20,6 +21,15 @@ function doxToMD(items) {
     .map(item => ({
       namespace: NAMESPACES[item.ctx.constructor],
       name: item.ctx.name,
+      description: item.description.full.replace(`<br />`, ` `),
+      arguments: item.tags
+        .filter(arg => !!arg.name)
+        .map(arg => ({
+          name: trim(arg.name, `[]`),
+          description: arg.description,
+          required: !arg.name.startsWith(`[`),
+          types: arg.types.join(` or `),
+        })),
     }))
     ;
 }
