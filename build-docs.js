6f8b8493ac18216d4184b587f2cc4b95d0eba963
teddddd <ted@mixpanel.com>
2019-03-11 18:26:57 -0700

    filter down dox items to the ones that interest us

--- build-docs.js
+++ build-docs.js
@@ -3,7 +3,25 @@ const fs = require(`fs`);
 const path = require(`path`);
 
 
+const DOCUMENTED = new Set([
+  `MixpanelLib`,
+  `MixpanelPeople`,
+  `MixpanelGroup`,
+]);
+
+function doxToMD(items) {
+  console.log(`before`, items.length);
+  items = items.filter(item =>
+    !item.isPrivate &&
+    item.ctx &&
+    !item.ctx.name.startsWith(`_`) &&
+    DOCUMENTED.has(item.ctx.constructor)
+  );
+  console.log(`after`, items.length);
+  return 'PLACEHOLDER';
+}
+
 const rawCode = fs.readFileSync(path.join(__dirname, `mixpanel.js`)).toString().trim();
 const parsed = dox.parseComments(rawCode);
 
-console.log(parsed);
+console.log(doxToMD(parsed));
