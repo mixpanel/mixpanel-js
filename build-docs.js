f00c95bee870e327c33f85c1c2d618a5f83389e4
teddddd <ted@mixpanel.com>
2019-03-12 13:34:48 -0700

    WIP produce MD from lodash template

--- build-docs.js
+++ build-docs.js
@@ -1,6 +1,6 @@
 const dox = require(`dox`);
 const fs = require(`fs`);
-const {trim} = require(`lodash`);
+const {template, trim} = require(`lodash`);
 const path = require(`path`);
 
 
@@ -11,27 +11,29 @@ const NAMESPACES = {
 };
 
 function doxToMD(items) {
-  return items
-    .filter(item =>
-      !item.isPrivate &&
-      item.ctx &&
-      !item.ctx.name.startsWith(`_`) &&
-      !!NAMESPACES[item.ctx.constructor]
-    )
-    .map(item => ({
-      namespace: NAMESPACES[item.ctx.constructor],
-      name: item.ctx.name,
-      description: item.description.full.replace(`<br />`, ` `),
-      arguments: item.tags
-        .filter(arg => !!arg.name)
-        .map(arg => ({
-          name: trim(arg.name, `[]`),
-          description: arg.description,
-          required: !arg.name.startsWith(`[`),
-          types: arg.types.join(` or `),
-        })),
-    }))
-    ;
+  const renderMD = template(`## <hr><span style="font-family: courier"><%- items.length %></span>`);
+  return renderMD({
+    items: items
+      .filter(item =>
+        !item.isPrivate &&
+        item.ctx &&
+        !item.ctx.name.startsWith(`_`) &&
+        !!NAMESPACES[item.ctx.constructor]
+      )
+      .map(item => ({
+        namespace: NAMESPACES[item.ctx.constructor],
+        name: item.ctx.name,
+        description: item.description.full.replace(`<br />`, ` `),
+        arguments: item.tags
+          .filter(arg => !!arg.name)
+          .map(arg => ({
+            name: trim(arg.name, `[]`),
+            description: arg.description,
+            required: !arg.name.startsWith(`[`),
+            types: arg.types.join(` or `),
+          })),
+      }))
+  });
 }
 
 const rawCode = fs.readFileSync(path.join(__dirname, `mixpanel.js`)).toString().trim();
