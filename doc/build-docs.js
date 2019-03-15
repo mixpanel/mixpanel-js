be2c56517cdd9441bd270763b6a16c57363855f2
teddddd <ted@mixpanel.com>
2019-03-15 15:43:25 -0700

    add a few explanatory comments to doc generator

--- doc/build-docs.js
+++ doc/build-docs.js
@@ -1,3 +1,14 @@
+/**
+ * A tool to generate Markdown documentation from JSDoc comments in the SDK source,
+ * in a format suited for publication on readme.io.
+ *
+ * The general pipeline is:
+ *   bundled source (mixpanel.js) -> dox -> data transformation -> .md template -> compiled .md
+ *
+ * The rendered doc is published with https://www.npmjs.com/package/rdme and lives at
+ * https://developer.mixpanel.com/docs/javascript-full-api-reference
+ */
+
 const dox = require(`dox`);
 const fs = require(`fs`);
 const {mapValues, template, trim} = require(`lodash`);
@@ -14,12 +25,13 @@ const NAMESPACES = {
   MixpanelGroup: `mixpanel.group`,
 };
 
+// dox generates some HTML markup automatically out of JSDoc description blocks. We parse a few
+// sections out of these blocks with the following RegExps
 const DESCRIPTION_REGEXES = {
   description: /([\S\s]+?)(<h3>|$)/,
   usage: /<h3>Usage:<\/h3>([\S\s]+?)(<h3>|$)/,
   notes: /<h3>Notes:<\/h3>([\S\s]+?)(<h3>|$)/,
 };
-
 function parseDescriptionAttrs(html) {
   return mapValues(DESCRIPTION_REGEXES, regex => {
     const match = html.match(regex);
@@ -32,19 +44,28 @@ function parseDescriptionAttrs(html) {
   });
 }
 
+// transform the structured data dox parses out of our JSDoc and feed it through
+// the lodash template at template.md
 function doxToMD(items) {
   const renderMD = template(fs.readFileSync(TEMPLATE_FILE).toString());
   return renderMD({
     namespaces: Object.entries(NAMESPACES).map(([constructor, namespace]) => ({
       name: namespace,
       items: items
+
+        // filter down to public methods of the current namespace/class
         .filter(item =>
           !item.isPrivate &&
           item.ctx &&
           !item.ctx.name.startsWith(`_`) &&
           item.ctx.constructor === constructor
         )
+
+        // sort by method name within each namespace
         .sort((a, b) => a.ctx.name > b.ctx.name ? 1 : -1)
+
+        // transform each method's data into the format we want (for instance stripping out
+        // <p> tags and adding a `required` field rather than [] around param names)
         .map(item => ({
           name: `${namespace}.${item.ctx.name}`,
           arguments: item.tags
