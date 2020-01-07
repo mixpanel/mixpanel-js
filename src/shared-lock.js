3e4dee0cad95612720cb6020cd5890d19c12fac9
teddddd <ted@mixpanel.com>
2020-01-07 22:18:24 +0000

    implement withLock

--- src/shared-lock.js
+++ src/shared-lock.js
@@ -27,8 +27,86 @@ var SharedLock = function(key, options) {
     this.timeoutMS = options.timeoutMS || 2000;
 };
 
-SharedLock.prototype.withLock = function() {
-    console.log('Hello world');
+SharedLock.prototype.withLock = function(lockedCB) {
+    var i = +(new Date()) + '|' + Math.random();
+    var startTime = +(new Date());
+
+    var key = this.storageKey;
+    var pollIntervalMS = this.pollIntervalMS;
+    var timeoutMS = this.timeoutMS;
+    var storage = this.storage;
+
+    var keyX = key + ':X';
+    var keyY = key + ':Y';
+    var keyZ = key + ':Z';
+
+    var delay = function(cb) {
+        if ((new Date()) - startTime > timeoutMS) {
+            console.error('Timeout waiting for mutex on ' + key + '; clearing lock.');
+            storage.removeItem(keyZ);
+            storage.removeItem(keyY);
+            loop();
+            return;
+        }
+        setTimeout(cb, pollIntervalMS * (Math.random() + 0.1));
+    };
+
+    var waitFor = function(predicate, cb) {
+        if (predicate()) {
+            cb();
+        } else {
+            delay(function() {
+                waitFor(predicate, cb);
+            });
+        }
+    };
+
+    var getSetY = function() {
+        if (storage.getItem(keyY)) {
+            return false;
+        } else {
+            storage.setItem(keyY, i);
+            return true;
+        }
+    };
+
+    var loop = function() {
+        storage.setItem(keyX, i);
+
+        waitFor(getSetY, function() {
+            if (storage.getItem(keyX) === i) {
+                criticalSection();
+                return;
+            }
+
+            delay(function() {
+                if (storage.getItem(keyY) !== i) {
+                    loop();
+                    return;
+                }
+                waitFor(function() {
+                    return !storage.getItem(keyZ);
+                }, criticalSection);
+            });
+        });
+    };
+
+    var criticalSection = function() {
+        storage.setItem(keyZ, '1');
+        try {
+            lockedCB();
+        } finally {
+            storage.removeItem(keyZ);
+            if (storage.getItem(keyY) === i) {
+                storage.removeItem(keyY);
+            }
+            if (storage.getItem(keyX) === i) {
+                storage.removeItem(keyX);
+            }
+        }
+    };
+
+    loop();
 };
 
 export { SharedLock };
