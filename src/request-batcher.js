1236974e97af9aa99511c279262086a6f3d9a1df
teddddd <ted@mixpanel.com>
2020-04-24 00:15:03 +0000

    more code comments for RequestQueue and RequestBatcher

--- src/request-batcher.js
+++ src/request-batcher.js
@@ -7,12 +7,13 @@ var MAX_RETRY_INTERVAL_MS = 10 * 60 * 1000; // 10 minutes
 var logger = console_with_prefix('batch');
 
 /**
- * RequestBatcher: manages the queueing, flushing, retry etc of requests of one type (events, people, groups).
+ * RequestBatcher: manages the queueing, flushing, retry etc of requests of one
+ * type (events, people, groups).
  * Uses RequestQueue to manage the backing store.
  * @constructor
  */
-var RequestBatcher = function(key, endpoint, options) {
-    this.queue = new RequestQueue(key);
+var RequestBatcher = function(storageKey, endpoint, options) {
+    this.queue = new RequestQueue(storageKey);
     this.endpoint = endpoint;
 
     this.libConfig = options.libConfig;
@@ -25,14 +26,25 @@ var RequestBatcher = function(key, endpoint, options) {
     this.stopped = false;
 };
 
+/**
+ * Add one item to queue.
+ */
 RequestBatcher.prototype.enqueue = function(item, cb) {
     this.queue.enqueue(item, this.flushInterval, cb);
 };
 
+/**
+ * Start flushing batches at the configured time interval. Must call
+ * this method upon SDK init in order to send anything over the network.
+ */
 RequestBatcher.prototype.start = function() {
+    this.stopped = false;
     this.flush();
 };
 
+/**
+ * Stop flushing batches. Can be restarted by calling start().
+ */
 RequestBatcher.prototype.stop = function() {
     this.stopped = true;
     if (this.timeoutID) {
@@ -41,18 +53,30 @@ RequestBatcher.prototype.stop = function() {
     }
 };
 
+/**
+ * Clear out queue.
+ */
 RequestBatcher.prototype.clear = function() {
     this.queue.clear();
 };
 
+/**
+ * Restore batch size configuration to whatever is set in the main SDK.
+ */
 RequestBatcher.prototype.resetBatchSize = function() {
     this.batchSize = this.libConfig['batch_size'];
 };
 
+/**
+ * Restore flush interval time configuration to whatever is set in the main SDK.
+ */
 RequestBatcher.prototype.resetFlush = function() {
-    this.scheduleFlush(this.libConfig['batch_flush_interval_ms']); // restore to configured flush interval
+    this.scheduleFlush(this.libConfig['batch_flush_interval_ms']);
 };
 
+/**
+ * Schedule the next flush in the given number of milliseconds.
+ */
 RequestBatcher.prototype.scheduleFlush = function(flushMS) {
     this.flushInterval = flushMS;
     if (!this.stopped) { // don't schedule anymore if batching has been stopped
@@ -60,6 +84,16 @@ RequestBatcher.prototype.scheduleFlush = function(flushMS) {
     }
 };
 
+/**
+ * Flush one batch to network. Depending on success/failure modes, it will either
+ * remove the batch from the queue or leave it in for retry, and schedule the next
+ * flush. In cases of most network or API failures, it will back off exponentially
+ * when retrying.
+ * @param {Object} [options]
+ * @param {boolean} [options.sendBeacon] - whether to send batch with
+ * navigator.sendBeacon (only useful for sending batches before page unloads, as
+ * sendBeacon offers no callbacks or status indications)
+ */
 RequestBatcher.prototype.flush = function(options) {
     try {
 
