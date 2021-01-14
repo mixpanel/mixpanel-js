3944f0ee3142c6c6c7c7d64cc5465e950629923c
teddddd <ted@mixpanel.com>
2021-01-14 22:04:24 +0000

    add a bunch of (failing) test cases for blockable bot UAs

--- tests/test.js
+++ tests/test.js
@@ -3697,16 +3697,31 @@
                 same(mixpanel._.info.browserVersion(a), 5.2);
             });
 
-            test('blocked user agents', 5, function() {
+            test('blocked user agents', function() {
                 var bot_user_agents = [
                     "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
                     "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+                    "Mozilla/5.0 (Windows Phone 8.1; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 530) like Gecko (compatible; adidxbot/2.0; +http://www.bing.com/bingbot.htm)",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534+ (KHTML, like Gecko) BingPreview/1.0b",
                     "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
                     "Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)",
-                    "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)"
+                    "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
+                    "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
+                    "facebookexternalhit/1.1",
+                    "Mozilla/5.0 (compatible; Pinterestbot/1.0; +http://www.pinterest.com/bot.html)",
+                    "APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)",
+                    "Mediapartners-Google",
+                    "Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)",
+                    "FeedFetcher-Google; (+http://www.google.com/feedfetcher.html)",
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36 (compatible; Google-Read-Aloud; +/search/docs/advanced/crawling/overview-google-crawlers)",
+                    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012; DuplexWeb-Google/1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Mobile Safari/537.36",
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36 Google Favicon",
+                    "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19",
+                    "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012; Storebot-Google/1.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
+                    "Screaming Frog SEO Spider/12.3"
                 ];
                 _.each(bot_user_agents, function(ua) {
-                    ok(mixpanel._.isBlockedUA(ua));
+                    ok(mixpanel._.isBlockedUA(ua), ua);
                 });
             });
 
