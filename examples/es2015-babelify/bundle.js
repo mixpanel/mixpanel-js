8d191e5a8fca53f85fc72b54ccdfcb04669c4ed4
teddddd <ted@mixpanel.com>
2021-11-10 00:15:28 +0000

    build 2.42.0

--- examples/es2015-babelify/bundle.js
+++ examples/es2015-babelify/bundle.js
@@ -163,7 +163,7 @@ Object.defineProperty(exports, '__esModule', {
 });
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.42.0-rc2'
+    LIB_VERSION: '2.42.0'
 };
 
 exports['default'] = Config;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.42.0-rc2'
+    LIB_VERSION: '2.42.0'
 };
 
 export default Config;
