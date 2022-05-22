cf6a65035798c4f3e726458836a3736f868bce78
Shelly Jaglan <shelly.jaglan_cont@meesho.com>
2022-05-22 19:18:25 +0530

    global

--- dist/mixpanel.globals.js
+++ dist/mixpanel.globals.js
@@ -60,10 +60,12 @@
     var console = {
         /** @type {function(...*)} */
         log: function() {
+            console.log("here argumensts in log func: ", arguments);
             if (Config.DEBUG && !_.isUndefined(windowConsole) && windowConsole) {
                 try {
                     windowConsole.log.apply(windowConsole, arguments);
                 } catch (err) {
+                    console.log("here argumensts in log func error catch: ", err);
                     _.each(arguments, function(arg) {
                         windowConsole.log(arg);
                     });
