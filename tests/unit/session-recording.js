41bc7b78b407f1066e6efdc791360819aa8ea376
Jakub Grzegorzewski <jakewski@gmail.com>
2024-10-23 18:39:51 +0000

    unit test comment, remove import

--- tests/unit/session-recording.js
+++ tests/unit/session-recording.js
@@ -1,14 +1,10 @@
 /*
- * Test that basic SDK usage (init, track, etc) does not
- * blow up in non-browser (node.js) envs. These are not
- * tests of server-side tracking functionality (which is
- * currently not supported in the browser lib).
+ * Unit tests for the SessionRecording module, excluding the rrweb dependency.
  */
 
 import { expect } from 'chai';
 import { SessionRecording } from '../../src/recorder/session-recording';
 import sinon from 'sinon';
-import localStorage from 'localStorage';
 
 describe(`SessionRecording`, function() {
   beforeEach(function() {
