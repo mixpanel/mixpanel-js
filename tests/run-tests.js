4f6d9892d5f4b2d607d76979ec3e34d0ee783139
Jakub Grzegorzewski <jakewski@gmail.com>
2024-12-05 15:58:21 +0000

    slight hack for qunit filter to work

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -19,10 +19,19 @@
                 }
             });
     });
-    window.initPromise = initPromise;
-    var testAsyncPromise = new Promise(function(resolve) {
-        testAsync(window.mixpanel, resolve);
-    });
+
+    // QUnit won't be able to find the test from testMixpanel using the ?filter query param
+    // if it has to wait for testAsync to complete - which we want to do to avoid test leakage into testMixpanel.
+    // (e.g. a request fires from testAsync and gets caught in one of testMixpanel's request stubs)
+    // Since the vast majority of tests are in testMixpanel and testAsync runs at the beginning, just skip testAsync when there's a filter
+    var testAsyncPromise;
+    if (!QUnit.config.filter) {
+        testAsyncPromise = new Promise(function(resolve) {
+            testAsync(window.mixpanel, resolve);
+        });
+    } else {
+        testAsyncPromise = Promise.resolve();
+    }
 
     testAsyncPromise
         .then(function() {
