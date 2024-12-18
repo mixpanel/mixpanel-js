b9fdf6a735695fc1c1f7ba1df0e9b9b3fcf5a9b0
Carlos Trevino <35301701+carlosdanieltt@users.noreply.github.com>
2024-12-18 19:18:08 +0000

    v2.58.0

--- dist/mixpanel.globals.js
+++ dist/mixpanel.globals.js
@@ -3,7 +3,7 @@
 
     var Config = {
         DEBUG: false,
-        LIB_VERSION: '2.58.0-rc1'
+        LIB_VERSION: '2.58.0'
     };
 
     // since es6 imports are static and we run unit tests from the console, window won't be defined when importing this file
--- examples/es2015-babelify/bundle.js
+++ examples/es2015-babelify/bundle.js
@@ -10982,7 +10982,7 @@ Object.defineProperty(exports, '__esModule', {
 });
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.58.0-rc1'
+    LIB_VERSION: '2.58.0'
 };
 
 exports['default'] = Config;
