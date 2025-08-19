94da2ab4c2862391ec112e7341a022813a602aee
Jakub Grzegorzewski <25271819+jakewski@users.noreply.github.com>
2025-08-19 23:02:46 +0000

    add same test but cookie

--- tests/test.js
+++ tests/test.js
@@ -869,6 +869,30 @@
                     start();
                 });
             });
+
+            test("when disable persistence is true, `init` should make no calls to browser storage APIs (cookie/localstorage/sessionstorage/idb)", 4, function() {
+                var idbOpenSpy = sinon.spy(window.indexedDB, `open`);
+                var localStorageSetItemSpy = sinon.spy(window.localStorage, `setItem`);
+                var sessionStorageSetItemSpy = sinon.spy(window.sessionStorage, `setItem`);
+                var originalCookie = document.cookie;
+
+                mixpanel.init('lib3', {
+                    persistence: 'cookie',
+                    persistence_name: name,
+                    batch_requests: false,
+                    disable_persistence: true
+                }, 'lib3');
+
+                stop();
+                setTimeout(function() {
+                    same(idbOpenSpy.callCount, 0, "IDB should not be opened");
+                    same(localStorageSetItemSpy.callCount, 0, "localStorage.setItem should not be called");
+                    same(sessionStorageSetItemSpy.callCount, 0, "sessionStorage.setItem should not be called");
+                    same(document.cookie, originalCookie, "document.cookie shouldn't have been changed");
+                    sinon.restore();
+                    start();
+                }, 500);
+            });
         }
 
         if (window.localStorage) {
