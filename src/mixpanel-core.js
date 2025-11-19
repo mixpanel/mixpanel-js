393c81df9541e9f1d3b6b79f247c118aa525defe
Patrick Huang <patrick.huang@mixpanel.com>
2025-11-19 15:11:57 -0800

    hooks are all consistent now

--- src/mixpanel-core.js
+++ src/mixpanel-core.js
@@ -1,6 +1,6 @@
 /* eslint camelcase: "off" */
 import Config from './config';
-import { MAX_RECORDING_MS, _, console, userAgent, document, navigator, slice, NOOP_FUNC, JSONStringify, send_sdk_extension_message } from './utils';
+import { MAX_RECORDING_MS, _, console, userAgent, document, navigator, NOOP_FUNC, JSONStringify, send_sdk_extension_message } from './utils';
 import { isRecordingExpired } from './recorder/utils';
 import { window } from './window';
 import { Autocapture } from './autocapture';
@@ -227,6 +227,7 @@ var create_mplib = function(token, config, name) {
         source: source,
         token: token,
         name: name,
+        info: _.info,
     });
 
     // if target is not defined, we called init after the lib already
@@ -1048,8 +1049,14 @@ MixpanelLib.prototype._track_or_batch = function(options, callback) {
  * with the tracking payload sent to the API server is returned; otherwise false.
  */
 MixpanelLib.prototype.track = addOptOutCheckMixpanelLib(function(event_name, properties, options, callback) {
-    const run_hook_args = ['before_track'].concat(Array.prototype.slice.call(arguments));
-    this._run_hook.apply(this, run_hook_args);
+    var ret = this._run_hook('before_track', {event_name: event_name, properties: properties, options: options, callback: callback});
+    if (ret === null) {
+        return;
+    }
+    event_name = ret.event_name;
+    properties = ret.properties;
+    options = ret.options;
+    callback = ret.callback;
 
     if (!callback && typeof options === 'function') {
         callback = options;
@@ -1120,7 +1127,7 @@ MixpanelLib.prototype.track = addOptOutCheckMixpanelLib(function(event_name, pro
         'event': event_name,
         'properties': properties
     };
-    var ret = this._track_or_batch({
+    ret = this._track_or_batch({
         type: 'events',
         data: data,
         endpoint: this.get_api_host('events') + '/' + this.get_config('api_routes')['track'],
@@ -1466,8 +1473,13 @@ var options_for_register = function(days_or_options) {
  * @param {boolean} [days_or_options.persistent=true] - whether to put in persistent storage (cookie/localStorage)
  */
 MixpanelLib.prototype.register = function(props, days_or_options) {
-    const run_hook_args = ['before_register'].concat(Array.prototype.slice.call(arguments));
-    this._run_hook.apply(this, run_hook_args);
+    var ret = this._run_hook('before_register', {properties: props, days_or_options: days_or_options});
+    if (ret === null) {
+        return;
+    }
+    props = ret.properties;
+    days_or_options = ret.days_or_options;
+
 
     var options = options_for_register(days_or_options);
     if (options['persistent']) {
@@ -1505,8 +1517,13 @@ MixpanelLib.prototype.register = function(props, days_or_options) {
  * @param {boolean} [days_or_options.persistent=true] - whether to put in persistent storage (cookie/localStorage)
  */
 MixpanelLib.prototype.register_once = function(props, default_value, days_or_options) {
-    const run_hook_args = ['before_register_once'].concat(Array.prototype.slice.call(arguments));
-    this._run_hook.apply(this, run_hook_args);
+    var ret = this._run_hook('before_register_once', {properties: props, default_value: default_value, days_or_options: days_or_options});
+    if (ret === null) {
+        return;
+    }
+    props = ret.properties;
+    default_value = ret.default_value;
+    days_or_options = ret.days_or_options;
 
     var options = options_for_register(days_or_options);
     if (options['persistent']) {
@@ -1531,8 +1548,12 @@ MixpanelLib.prototype.register_once = function(props, default_value, days_or_opt
  * @param {boolean} [options.persistent=true] - whether to look in persistent storage (cookie/localStorage)
  */
 MixpanelLib.prototype.unregister = function(property, options) {
-    const run_hook_args = ['before_unregister'].concat(Array.prototype.slice.call(arguments));
-    this._run_hook.apply(this, run_hook_args);
+    var ret = this._run_hook('before_unregister', {property: property, options: options});
+    if (ret === null) {
+        return;
+    }
+    property = ret.property;
+    options = ret.options;
 
     options = options_for_register(options);
     if (options['persistent']) {
@@ -1582,8 +1603,28 @@ MixpanelLib.prototype.identify = function(
     //  _set_once_callback:function  A callback to be run if and when the People set_once queue is flushed
     //  _union_callback:function  A callback to be run if and when the People union queue is flushed
     //  _unset_callback:function  A callback to be run if and when the People unset queue is flushed
-    const run_hook_args = ['before_identify'].concat(Array.prototype.slice.call(arguments));
-    this._run_hook.apply(this, run_hook_args);
+    var hook_args = {
+        unique_id: new_distinct_id,
+        _set_callback: _set_callback,
+        _add_callback: _add_callback,
+        _append_callback: _append_callback,
+        _set_once_callback: _set_once_callback,
+        _union_callback: _union_callback,
+        _unset_callback: _unset_callback,
+        _remove_callback: _remove_callback,
+    };
+    var ret = this._run_hook('before_identify', hook_args);
+    if (ret === null) {
+        return -1;
+    }
+    new_distinct_id = ret.unique_id;
+    _set_callback = ret._set_callback;
+    _add_callback = ret._add_callback;
+    _append_callback = ret._append_callback;
+    _set_once_callback = ret._set_once_callback;
+    _union_callback = ret._union_callback;
+    _unset_callback = ret._unset_callback;
+    _remove_callback = ret._remove_callback;
 
     var previous_distinct_id = this.get_distinct_id();
     if (new_distinct_id && previous_distinct_id !== new_distinct_id) {
@@ -1941,17 +1982,18 @@ MixpanelLib.prototype.get_config = function(prop_name) {
  * Fetch a hook function from config, with safe default, and run it
  * against the given arguments
  * @param {string} hook_name which hook to retrieve
+ * @param {Object} hook_args which are passed into the retrieved hook
  * @returns {any|null} return value of user-provided hook, or null if nothing was returned
  */
-MixpanelLib.prototype._run_hook = function(hook_name) {
-    var ret = slice.call(arguments, 1);
+MixpanelLib.prototype._run_hook = function(hook_name, hook_args) {
+    var ret = hook_args;
     _.each(this.hooks[hook_name], function(hook) {
         if(ret === null) {
-            return ret;
+            return null;
         }
-        ret = hook.apply(this, ret);
-        if (typeof ret === 'undefined') {
-            this.report_error(hook_name + ' hook did not return a value');
+        ret = hook.call(this, ret);
+        if (typeof ret === 'undefined' || !_.isObject(ret)) {
+            this.report_error(hook_name + ' hook did not return a valid value');
             ret = null;
         }
     }, this);
