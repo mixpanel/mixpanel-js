aad451f0e039b4d67f157b4ad6a6c6e6b04e9c3a
Patrick Huang <212442744+phuang-mixpanel@users.noreply.github.com>
2025-11-20 01:43:05 +0000

    readding the comment

--- src/window.js
+++ src/window.js
@@ -1,3 +1,4 @@
+// since es6 imports are static and we run unit tests from the console, window won't be defined when importing this file
 var win;
 if (typeof(window) === 'undefined') {
     var loc = {
