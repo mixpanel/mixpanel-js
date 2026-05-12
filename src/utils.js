6c970ad203e5ed287e7b543efbd40f1f10627e0d
efahk <37164746+efahk@users.noreply.github.com>
2026-05-12 14:44:56 -0700

    Add warn to console_with_prefix func (#448)

--- src/utils.js
+++ src/utils.js
@@ -104,6 +104,7 @@ var log_func_with_prefix = function(func, prefix) {
 var console_with_prefix = function(prefix) {
     return {
         log: log_func_with_prefix(console.log, prefix),
+        warn: log_func_with_prefix(console.warn, prefix),
         error: log_func_with_prefix(console.error, prefix),
         critical: log_func_with_prefix(console.critical, prefix)
     };
