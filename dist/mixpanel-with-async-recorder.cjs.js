1a970a267e6f730a46e6cc9ce0bd9bf2b6f5b628
Jakub Grzegorzewski <25271819+jakewski@users.noreply.github.com>
2026-07-09 20:20:37 +0000

    changelog and version bump

--- dist/mixpanel-with-async-recorder.cjs.js
+++ dist/mixpanel-with-async-recorder.cjs.js
@@ -2,7 +2,7 @@
 
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.80.0'
+    LIB_VERSION: '2.81.0'
 };
 
 // Window global names for async modules
@@ -10,8 +10,8 @@ var TARGETING_GLOBAL_NAME = '__mp_targeting';
 var RECORDER_GLOBAL_NAME = '__mp_recorder';
 
 // Constants that are injected at build-time for the names of async modules.
-var RECORDER_FILENAME = 'mixpanel-recorder-C3AW7mPl.js';
-var TARGETING_FILENAME = 'mixpanel-targeting-CBwOQJZw.js';
+var RECORDER_FILENAME = 'mixpanel-recorder-C0B3sEK2.js';
+var TARGETING_FILENAME = 'mixpanel-targeting-ok0mwgIw.js';
 
 // since es6 imports are static and we run unit tests from the console, window won't be defined when importing this file
 var win;
