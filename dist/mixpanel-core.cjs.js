81ce0f878fbc23589e6f8034a7ea23e58449d79d
Jakub Grzegorzewski <25271819+jakewski@users.noreply.github.com>
2026-08-10 17:09:29 +0000

    non-rc build

--- dist/mixpanel-core.cjs.js
+++ dist/mixpanel-core.cjs.js
@@ -2,7 +2,7 @@
 
 var Config = {
     DEBUG: false,
-    LIB_VERSION: '2.82.0-rc1'
+    LIB_VERSION: '2.82.0'
 };
 
 // Window global names for async modules
--- dist/mixpanel-targeting.js
+++ dist/mixpanel-targeting.js
@@ -27,7 +27,7 @@
     }
 
     var Config = {
-        LIB_VERSION: '2.82.0-rc1'
+        LIB_VERSION: '2.82.0'
     };
 
     // Window global names for async modules
